e05019f8b3992e41d1a15309a2457de896d42d81
Scott Matthews <scottmat@broadinstitute.org>
2014-05-28 09:31:49 -0400

    Adding root JavaFX Application to run the Game

---
+++ src/Battleship.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * TODO scottmat fill in javadoc!!!
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Battleship extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
